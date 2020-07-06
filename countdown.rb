ee03d77eda7c7e6a2cbe770512f788f63c594d8a
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-06 21:18:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,3 +2,26 @@
 
 def countdown
 end
+def countdown(num)
+
+ while num > 0
+
+   puts "#{num} SECOND(S)!"
+  num -= 1
+
+ end
+  "HAPPY NEW YEAR!"
+end
+
+
+ def countdown_with_sleep(num)
+
+ while num > 0
+
+   puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+ end
+  "HAPPY NEW YEAR!"
+end	
