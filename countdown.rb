146c1515c4c0c8cfb1f98dfceee0aeda793ca0fb
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-06 21:19:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
 def countdown(x)
-while x > 0
-  puts "#{x} SECOND(S)!"
-  sleep 1
-  x -= 1
-end
+  while x > 0
+   puts "#{x} SECOND(S)!"
+    sleep 1
+   x -= 1
+  end
 return "HAPPY NEW YEAR!"
 end
 
